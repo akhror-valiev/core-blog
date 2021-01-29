8b37d703248ad148ed6f7000c7f0ae54148bc8b9
akhror-valiev <ahror.valiev@gmail.com>
2021-01-29 21:23:00 +0500

    ckeditor in the models is rm

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
-from ckeditor.fields import RichTextField
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
