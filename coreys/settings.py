1236c6f41fbd41b0ff5cbbd82326c782b95e1db3
akhror-valiev <ahror.valiev@gmail.com>
2021-01-29 21:53:37 +0500

    allowed host

--- coreys/settings.py
+++ coreys/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = '#$)9#*g0j-g_@c298$v&beyvp1g!weqye*s@h^f6&kw($=-(si'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sim-blog.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
